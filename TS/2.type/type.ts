5bd15178d51d6313ba966338a00105d29c7e69eb
youngha123 <dudgk7829@naver.com>
2024-08-09 12:21:19 +0900

    20240709

--- TS/2.type/type.ts
+++ TS/2.type/type.ts
@@ -58,27 +58,41 @@ funcType = func2;
 
 /*
     타입추론 :
-    - 
+    - 타입을 선언하지 않고 변수에 값이 초기화되는 순간에 할당된 값에 따라서 변수의 타입이 결정 
 */
+let stringVariable = '';
+// stringVariable = 10;
 
-
+const func3 = (arg: number) => {
+    if (arg === 1) return 1;
+    return '';
+}
 
 /*
     타입 변환 :
-    - 
+    - as 키워드로 형변환 가능 (객체일때 자주 사용), <> 연산자로 형변환 가능
 */
+const PI: any = '3.14';
+let piNumber: number = PI as number;
+piNumber = <number>PI;
 
-
+piNumber = Number(PI);
 
 /*
     유니온 타입
-    - 
+    - 복수의 타입을 지정하는 방법, | 사용
 */
-
-
+let union: number | string;
+union = 10;
+union = '10';
+// union = false;
+// const number1: number = union as number; // number1에 union 값을 넣고싶으면 'as 해당 객체' 사용 
 
 /*
     리터럴 타입
-    - 
+    - 지정한 리터럴 값만 가질 수 있는 타입
 */
-
+let day: '낮' | '밤';
+day = '낮';
+day = '밤';
+// day = '아침';
